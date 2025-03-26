f62e5f4d318c08332391ca57e8806df4d04380c7
Murad Sawaged <muradsawaged@Murads-MacBook-Pro.local>
2025-03-26 00:56:46 -0600

    Add Turbo Flip screen with countdown and real result logic

--- Documents/BinaryBetApp/App.js
+++ Documents/BinaryBetApp/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from './src/screens/HomeScreen';
+import TurboFlipScreen from './src/screens/TurboFlipScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TurboFlip" component={TurboFlipScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
